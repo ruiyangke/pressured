0fa5609b692bd019756694385d59d92704d5cec2
Ruiyang Ke <me@ry.ke>
2025-12-13 21:57:45 -0800

    format code

--- plugins/storage/crypto_mbedtls.c
+++ plugins/storage/crypto_mbedtls.c
@@ -12,7 +12,8 @@ void crypto_sha256(const void *data, size_t len, unsigned char *hash) {
 
 void crypto_hmac_sha256(const void *key, size_t key_len, const void *data,
                         size_t data_len, unsigned char *out) {
-  const mbedtls_md_info_t *md_info = mbedtls_md_info_from_type(MBEDTLS_MD_SHA256);
+  const mbedtls_md_info_t *md_info =
+      mbedtls_md_info_from_type(MBEDTLS_MD_SHA256);
   mbedtls_md_hmac(md_info, (const unsigned char *)key, key_len,
                   (const unsigned char *)data, data_len, out);
 }
